a1032a40648aa9885bc122bb2c76898bd479bcd8
Doug Franklin <doug@dougfranklin.com>
2021-03-01 11:29:55 -0500

    Sinon Added

---
+++ maths.js
@@ -0,0 +1,7 @@
+module.exports = Add;
+
+function Add(numberOne, numberTwo) {
+
+  return numberOne + numberTwo;
+
+}
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,9 @@
+var chai   = require('chai');
+var should = require('chai').should();
+var expect = chai.expect;
+var assert = chai.assert;
 
+var Add    = require('../maths');
 
 describe('Addition Tests', function() {
 
@@ -6,6 +11,16 @@ describe('Addition Tests', function() {
 
     var numberOne = 1;
     var numberTwo = 2;
+
+    var expectedResult = 3;
+    var notExpectedResult = 7;
+
+    var actualResult = Add(numberOne, numberTwo);
+
+    // actualResult.should.equal(expectedResult);
+    expect(actualResult).to.not.equal(notExpectedResult);
+
+    assert.equal(actualResult, expectedResult);
 
   });
 
